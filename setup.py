32596c304db8ca536d2ebf9c9b50a76b19b82cbc
Michael Bruckner <mbruckner1996@gmail.com>
2016-02-11 16:34:24 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='CSV-Projekt',
+        version='3.5',
+        packages=[''],
+        url='https://github.com/brucki2811/CSV-Projekt',
+        license='GPL',
+        author='michaelbruckner',
+        author_email='mbruckner@student.tgm.ac.at',
+        description=''
+)
